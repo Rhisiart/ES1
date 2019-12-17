50637e6504b8e080d48aafec77639947c8341b4c
ricar <ricardobenfiquista13@gmail.com>
2019-12-17 10:50:18 +0000

    Sprint 4

--- src/PlacesManager.java
+++ src/PlacesManager.java
@@ -57,7 +57,7 @@ public class PlacesManager extends UnicastRemoteObject implements PlacesListInte
                 }
         }));
         t3.start();
-
+        
     }
 
     private void chooseLeader()  {
